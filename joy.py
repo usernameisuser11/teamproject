56c53d664bf3912679edf75bbb6951ffc5d73747
kds10111 <kds10111@gmail.com>
2025-04-18 13:37:02 +0900

    김동석석

--- joy.py
+++
@@ -1,7 +0,0 @@
-def delete_file(file_path):
-    try:
-        if os.path.exists(file_path):
-            os.remove(file_path)
-            print(f"{file_path} has been deleted.")
-        else:
-            print(f"{file_path} does not exist.")
