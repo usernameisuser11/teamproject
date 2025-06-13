66cdda5fbdfe0c6d4b1759cd635138c06e5850ad
friedwook <yoojw1118@gmail.com>
2025-06-13 13:50:12 +0900

    최종 파일명 정리

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
 import os
 from  sortingMails import sortingMails
-from Gmail_contents import test
+from Gmail_contents import GetMails
 from Gmail_contents import gmail2_mysql
 from Gmail_contents import decrypt
 def saveDB(Id,userName):
     credential_path = os.path.dirname(os.path.abspath(__file__))
     if not os.path.exists(os.path.join(credential_path, "Gmail_contents," "credential.json")) :
         decrypt.decryptCredential()
-    getmails = test.GmailReader()
+    getmails = GetMails.GmailReader()
     gotmails = []
     
     gotmails = getmails.get_emails(gotID=Id)
